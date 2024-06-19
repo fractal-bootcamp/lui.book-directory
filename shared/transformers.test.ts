831c4c6b4094975c3a3c36dbfec253cb590e0536
yab <hello@factions.ai>
2024-06-19 18:40:44 -0400

    6 x tests for all GET API calls

--- shared/transformers.test.ts
+++ shared/transformers.test.ts
@@ -1,7 +1,64 @@
 import { Author, sampleAuthor, Book, Tag, Webuser } from "./constants";
-import { getAuthor } from "./transformers";
+import {
+  getAllAuthors,
+  getAllBooks,
+  getAuthor,
+  getBook,
+  searchAuthors,
+  searchBooks,
+} from "./transformers";
 import { test, describe, it, expect } from "vitest";
 
-test("gets an Author object with the id", () => {
-  expect(getAuthor(999)).toBe(sampleAuthor);
+//////////////////////////////////////////////////////////////////////////////
+// GET ONE
+//////////////////////////////////////////////////////////////////////////////
+
+describe(getAuthor, () => {
+  it("gets an Author object with the id", async () => {
+    const author = await getAuthor(999);
+    expect(author).toEqual(sampleAuthor);
+  });
+});
+
+describe(getBook, () => {
+  it("gets an Author object with the id", async () => {
+    const author = await getBook(1);
+    expect(author).toBeTruthy();
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////
+// GET ALL
+//////////////////////////////////////////////////////////////////////////////
+
+describe(getAllAuthors, () => {
+  it("gets all Author objects", async () => {
+    const authors = await getAllAuthors();
+    expect(authors).toBeTruthy();
+  });
+});
+
+describe(getAllBooks, () => {
+  it("gets all Book objects", async () => {
+    const authors = await getAllBooks();
+    expect(authors).toBeTruthy();
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////
+// GET SEARCH
+//////////////////////////////////////////////////////////////////////////////
+
+describe(searchAuthors, () => {
+  it("searches across all Author objects", async () => {
+    const authors = await searchAuthors("Kurt");
+    expect(authors).toBeTruthy();
+  });
+});
+
+describe(searchBooks, () => {
+  it("searches across all Book objects", async () => {
+    const authors = await searchBooks("Atlas");
+    expect(authors).toBeTruthy();
+  });
 });
